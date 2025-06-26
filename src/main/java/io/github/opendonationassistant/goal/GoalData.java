665122a39a16bb406be5d20c72bb310e948d95d1
stCarolas <stcarolas@gmail.com>
2025-06-27 01:38:01 +0300

    up micronaut, fix nullable title

--- src/main/java/io/github/opendonationassistant/goal/GoalData.java
+++ src/main/java/io/github/opendonationassistant/goal/GoalData.java
@@ -1,9 +1,10 @@
 package io.github.opendonationassistant.goal;
 
+import io.micronaut.core.annotation.Nullable;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
 @MappedEntity("goal")
-public record GoalData(@Id String id, String title) {}
+public record GoalData(@Id String id, @Nullable String title) {}
