d6050fd8341dfc2d0a8a1e9be8022dcd7864c526
stCarolas <stcarolas@gmail.com>
2026-04-17 22:52:35 +0300

    refack jdbc configuration, add repeat command

--- src/main/java/io/github/opendonationassistant/Application.java
+++ src/main/java/io/github/opendonationassistant/Application.java
@@ -1,16 +1,20 @@
 package io.github.opendonationassistant;
 
+import com.rabbitmq.client.Channel;
 import io.github.opendonationassistant.rabbit.AMQPConfiguration;
 import io.github.opendonationassistant.rabbit.Exchange;
 import io.github.opendonationassistant.rabbit.Queue;
+import io.github.opendonationassistant.rabbit.RabbitClient;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextConfigurer;
 import io.micronaut.context.annotation.ContextConfigurer;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.serde.ObjectMapper;
 import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
+import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import java.util.List;
 import java.util.Map;
@@ -44,21 +48,28 @@ public class Application {
         Exchange.Exchange(
           "history",
           Map.of(
-            "event.ReelResultHistoryEvent", events,
-            "event.GoalHistoryEvent", events,
-            "event.MediaHistoryEvent", events,
-            "command", commands
-            )
+            "event.ReelResultHistoryEvent",
+            events,
+            "event.GoalHistoryEvent",
+            events,
+            "event.MediaHistoryEvent",
+            events,
+            "command",
+            commands
+          )
         ),
         Exchange.Exchange(
           "donaton",
           Map.of("event.DonatonDeadlineChanged", events)
         ),
-        Exchange.Exchange(
-          "actions",
-          Map.of("event.ActionHistoryEvent", events)
-        )
+        Exchange.Exchange("actions", Map.of("event.ActionHistoryEvent", events))
       )
     );
+  }
+
+  @Singleton
+  @Named("automation")
+  public RabbitClient automationFacade(Channel channel, ObjectMapper mapper) {
+    return new RabbitClient(channel, mapper, "automation");
   }
 }
